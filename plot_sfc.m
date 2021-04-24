8d68e45a000a2ab30cfe0c477b03eadf34ed4928
Zhengwu Zhang <zhengwustat@gmail.com>
2021-04-23 22:21:44 -0400

    add plot_sfc.m to illustrate how to plot sfc

---
+++ plot_sfc.m
@@ -0,0 +1,19 @@
+clear all;
+close all;
+
+addpath('./example_data');
+addpath('./io');
+addpath('./plotting')
+
+% load required SBCI data for mapping and analysis;
+% This gives mapping from current resolution to a higher or lower
+% resolution
+[sbci_parc, sbci_mapping, adjacency] = load_sbci_data('example_data', 0.94);
+
+% example to plot data on the inflated cortical surface
+sbci_surf = load_sbci_surface('example_data');
+
+
+%load Structural and Functional Coupling and plot it
+load example_sfc.mat
+plot_cortical(sbci_surf.inflated, sbci_mapping, sfc);
