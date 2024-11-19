152dc11cac9d162fa22dd8db3ca8d726a5980861
Adrian <aallen1@email.unc.edu>
2024-11-19 15:45:35 -0500

    Add parcellation overlay to plotting functions

--- plotting/plot_cortical.m
+++ plotting/plot_cortical.m
@@ -12,7 +12,9 @@
 %   data - (vector) A vector of data to plot according to the SBCI mapping
 %   varargin - Optional arguments (legend=title to place over colorbar,
 %      clim=limits for colorbar, cmap=colormap, bg=background color,
-%      figid=id of the generated figure, logscale=display colorbar in log scale
+%      figid=id of the generated figure, logscale=display colorbar in log
+%      scale, overlay_parc=parcellation structure for overlaying parcel
+%      boundaries
 %
 % OUTPUT:
 %   fig - (figure) handle to the resulting figure
@@ -33,6 +35,7 @@ addParameter(p, 'cmap', 'jet', @ischar);
 addParameter(p, 'bg', 'white', @ischar);
 addParameter(p, 'figid', 1, @(n)validateattributes(n,{'numeric'},{'nonnegative'}));
 addParameter(p, 'logscale', false, @islogical);
+addParameter(p, 'overlay_parc',[], @isstruct)
 
 % parse optional variables
 parse(p, varargin{:});
@@ -43,6 +46,14 @@ upsampled_data = zeros(sbci_map.shape(4), 1);
 
 for i = 1:length(data)
     upsampled_data(sbci_map.map(1, sbci_map.map(2,:) == i)) = data(i);
+end
+
+% Upsample overlay labels if present 
+if ~isempty(params.overlay_parc)
+    upsampled_overlay = zeros(sbci_map.shape(4), 1); 
+    for i = 1:length(params.overlay_parc.labels)
+        upsampled_overlay(sbci_map.map(1, sbci_map.map(2,:) == i)) = params.overlay_parc.labels(i);
+    end
 end
 
 % TODO: figure out nice values for these
@@ -61,6 +72,11 @@ ax(1) = axes('position', [0.07 0.3 w h]);
 trisurf(surf.tri, surf.vtx(1,:), surf.vtx(2,:), surf.vtx(3,:),...
    double(upsampled_data(1:sbci_map.shape(5))), 'EdgeColor', 'none');
 
+% Draw parcel boundaries
+if ~isempty(params.overlay_parc)
+    draw_boundary_lines(surf, upsampled_overlay(1:sbci_map.shape(5)));
+end
+
 view(-90,0)
 
 % options to make the plot look pretty
@@ -73,6 +89,10 @@ ax(2) = axes('position', [0.07+w+m 0.3 w h]);
 trisurf(surf.tri, surf.vtx(1,:), surf.vtx(2,:), surf.vtx(3,:),...
    double(upsampled_data(1:sbci_map.shape(5))), 'EdgeColor', 'none');
 
+% Draw parcel boundaries
+if ~isempty(params.overlay_parc)
+    draw_boundary_lines(surf, upsampled_overlay(1:sbci_map.shape(5)));
+end
 view(90,0)
 
 % options to make the plot look pretty
@@ -87,6 +107,10 @@ surf = sbci_surf.rh_surf;
 trisurf(surf.tri, surf.vtx(1,:), surf.vtx(2,:), surf.vtx(3,:),...
    double(upsampled_data((sbci_map.shape(5)+1):sbci_map.shape(4))), 'EdgeColor', 'none');
 
+% Draw parcel boundaries
+if ~isempty(params.overlay_parc)
+    draw_boundary_lines(surf, upsampled_overlay((sbci_map.shape(5)+1):sbci_map.shape(4)));
+end
 view(-90,0)
 
 % options to make the plot look pretty
@@ -99,6 +123,10 @@ ax(4) = axes('position', [0.07+3*(w+m) 0.3 w h]);
 trisurf(surf.tri, surf.vtx(1,:), surf.vtx(2,:), surf.vtx(3,:),...
    double(upsampled_data((sbci_map.shape(5)+1):sbci_map.shape(4))), 'EdgeColor', 'none');
 
+% Draw parcel boundaries
+if ~isempty(params.overlay_parc)
+    draw_boundary_lines(surf, upsampled_overlay((sbci_map.shape(5)+1):sbci_map.shape(4)));
+end
 view(90,0)
 
 % options to make the plot look pretty
@@ -106,7 +134,6 @@ daspect([1 1 1]); axis tight; camlight; axis vis3d off;
 lighting gouraud; material dull; shading interp;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 % ensure all plots have the same (and valid) colour range
 if params.clim(1) == params.clim(2)
@@ -131,9 +158,41 @@ handle = get(cb, 'Title');
 set(handle, 'String', params.legend);
 
 % set the background colour and set the aspect ratio
-whitebg(gcf, params.bg);
+set(gcf,'Color','white')
 pos = get(gcf, 'Position');
 set(gcf, 'Color', params.bg, 'InvertHardcopy', 'off', ...
     'Position', [pos(1), pos(2), pos(3), pos(3) / 2.5]);
 
+end
+
+% Function to draw parcel boundaries
+function draw_boundary_lines(surf, labels_to_display)
+    % Convert labels to per-triangle from per-vertex
+    tri_labels = mode(labels_to_display(surf.tri), 2);
+    
+    % Determine edges and triangles
+    edges = [surf.tri(:, [1, 2]); surf.tri(:, [2, 3]); surf.tri(:, [1, 3])];
+    edge_triangles = [(1:size(surf.tri, 1))'; (1:size(surf.tri, 1))'; (1:size(surf.tri, 1))'];
+    
+    % Sort edges 
+    [sorted_edges, sortIdx] = sortrows(sort(edges, 2));
+    sorted_edge_triangles = edge_triangles(sortIdx);
+    
+    % Find pairs of triangles that share an edge
+    diff_edges = diff(sorted_edges, 1, 1);
+    edge_diffs = sum(diff_edges, 2) == 0;
+    
+    % Triangles pairs sharing the same edge are adjacent
+    triangle_pairs = [sorted_edge_triangles([edge_diffs; false]), sorted_edge_triangles([false; edge_diffs])];
+    
+    % Filter pairs to those with different labels
+    diff_labels = tri_labels(triangle_pairs(:,1)) ~= tri_labels(triangle_pairs(:,2));
+    final_edges = sorted_edges([edge_diffs; false], :);
+    final_edges = final_edges(diff_labels, :);
+    
+    % Draw lines for these boundary edges
+    for i = 1:size(final_edges, 1)
+        vtx_indices = final_edges(i, :);
+        line(surf.vtx(1, vtx_indices), surf.vtx(2, vtx_indices), surf.vtx(3, vtx_indices), 'Color', 'k', 'LineWidth', 3);
+    end
 end
---
+++ plotting/plot_parce.m
@@ -0,0 +1,179 @@
+% MATLAB R2018a
+%
+% FUNCTION NAME:
+%   plot_parce
+%   
+% DESCRIPTION:
+%   visualize parcellation
+%
+% INPUT:
+%   sbci_surf - sbci_surf - (struct) A structure containing LH and RH meshes
+%   sbci_map - (struct) A structure containing SBCI mapping information
+%   labels - (vector) A vector with parcellation labels according to SBCI
+%   format, corresponding to the connectome cols or rows
+%   varargin - Optional arguments:
+%       my_cols - (vector) A Kx3 vector of colors for each parcel
+%       overlay_parc - (struct) A parcellation structure for overlaying
+%       parcel boundaries 
+%
+% SIDE-EFFECT: plot of the parcellation
+% OUTPUT:
+%   1
+%
+% ASSUMPTIONS AND LIMITATIONS:
+%   The data and mapping must come from the same run of the SBCI pipeline.
+%
+
+function result = plot_parce(sbci_surf, sbci_map, label,varargin)
+
+p = inputParser;
+addParameter(p, 'my_cols', []);
+addParameter(p, 'overlay_parc',[], @isstruct)
+
+% parse optional variables
+parse(p, varargin{:});
+params = p.Results;
+
+% Get colors for each parcel
+if isempty(params.my_cols)
+    lab_color = distinguishable_colors(length(label));
+else
+    lab_color = params.my_cols; 
+end
+
+% upsample data to the high resolution meshes
+upsampled_data = zeros(sbci_map.shape(4), 1);
+
+for i = 1:length(label)
+    upsampled_data(sbci_map.map(1, sbci_map.map(2,:) == i)) = label(i);
+end
+
+% Upsample overlay labels if present 
+if ~isempty(params.overlay_parc)
+    upsampled_overlay = zeros(sbci_map.shape(4), 1); 
+    for i = 1:length(params.overlay_parc.labels)
+        upsampled_overlay(sbci_map.map(1, sbci_map.map(2,:) == i)) = params.overlay_parc.labels(i);
+    end
+end
+
+%% %%%%%%%%%%left surface plot 1 %%%%%%%%%%%% %%
+surf = sbci_surf.lh_surf;
+subplot(2,2,1)
+ax1 = gca; 
+trisurf(surf.tri, surf.vtx(1,:), surf.vtx(2,:), surf.vtx(3,:),'EdgeColor',...
+    'none','Facecolor', 'k');
+title('Left Hemisphere - Front')
+daspect([1 1 1]); 
+axis tight; 
+view(90,23)
+
+labels_to_display = upsampled_data(1:sbci_map.shape(5));
+unique_labs = unique(labels_to_display);
+
+for i = 1:length(unique_labs)
+    hold on
+
+    %find tri corresponding to unique_labs(i)
+    vertex_lab = find(labels_to_display == unique_labs(i));  
+    for j=1:3
+        is_member(j,:) = ismember(surf.tri(:,j),vertex_lab);
+    end
+    surf_idx = sum(is_member)>1; 
+
+    trimesh(surf.tri(surf_idx,:), surf.vtx(1,:), surf.vtx(2,:), surf.vtx(3,:),...
+       'FaceColor', lab_color(unique_labs(i),:), 'EdgeColor',lab_color(unique_labs(i),:),'FaceAlpha',0.8);
+
+end
+if ~isempty(params.overlay_parc)
+    draw_boundary_lines(surf, upsampled_overlay(1:sbci_map.shape(5)));
+end
+axis off;
+
+%% %%%%%%%%%%left surface plot 2 %%%%%%%%%%%% %%
+subplot(2,2,3)
+ax2 = copyobj(ax1,gcf); 
+set(ax2, 'Position', get(subplot(2,2,3), 'Position'));
+title(ax2, 'Left Hemisphere - Back')
+view(ax2, 270, 0)
+axis off;
+
+%% %%%%%%%%%%right surface plot 1 %%%%%%%%%%%% %% 
+
+surf = sbci_surf.rh_surf;
+
+subplot(2,2,2)
+ax3 = gca; 
+trisurf(surf.tri, surf.vtx(1,:), surf.vtx(2,:), surf.vtx(3,:),'EdgeColor',...
+    'none','Facecolor', 'k');
+daspect([1 1 1]); 
+axis tight; 
+view(-90,23)
+title('Right Hemisphere - Front')
+
+labels_to_display = upsampled_data((sbci_map.shape(5)+1):sbci_map.shape(4));
+unique_labs = unique(labels_to_display);
+
+for i = 1:length(unique_labs)
+    hold on
+    
+    %find tri corresponding to unique_labs(i)
+    vertex_lab = find(labels_to_display == unique_labs(i));
+    
+    for j=1:3
+        is_member(j,:) = ismember(surf.tri(:,j),vertex_lab);
+    end
+    
+    surf_idx = sum(is_member)>1;   
+    trimesh(surf.tri(surf_idx,:), surf.vtx(1,:), surf.vtx(2,:), surf.vtx(3,:),...
+       'FaceColor', lab_color(unique_labs(i),:), 'EdgeColor',lab_color(unique_labs(i),:),'FaceAlpha',0.8);
+
+    
+end
+if ~isempty(params.overlay_parc)
+    draw_boundary_lines(surf, upsampled_overlay((sbci_map.shape(5)+1):sbci_map.shape(4)));
+end
+axis off;
+
+%% %%%%%%%%%%right surface plot 2 %%%%%%%%%%%% %% 
+
+subplot(2,2,4)
+ax4 = copyobj(ax3,gcf); 
+set(ax4, 'Position', get(subplot(2,2,4), 'Position'));
+title(ax4, 'Right Hemisphere - Back')
+view(ax4, 90, 0)
+axis off;
+
+result = 1;
+end
+
+% Function to draw parcel boundaries 
+function draw_boundary_lines(surf, labels_to_display)
+    % Convert labels to per-triangle from per-vertex
+    tri_labels = mode(labels_to_display(surf.tri), 2);
+    
+    % Determine edges and triangles
+    edges = [surf.tri(:, [1, 2]); surf.tri(:, [2, 3]); surf.tri(:, [1, 3])];
+    edge_triangles = [(1:size(surf.tri, 1))'; (1:size(surf.tri, 1))'; (1:size(surf.tri, 1))'];
+    
+    % Sort edges 
+    [sorted_edges, sortIdx] = sortrows(sort(edges, 2));
+    sorted_edge_triangles = edge_triangles(sortIdx);
+    
+    % Find pairs of triangles that share an edge
+    diff_edges = diff(sorted_edges, 1, 1);
+    edge_diffs = sum(diff_edges, 2) == 0;
+    
+    % Triangles pairs sharing the same edge are adjacent
+    triangle_pairs = [sorted_edge_triangles([edge_diffs; false]), sorted_edge_triangles([false; edge_diffs])];
+    
+    % Filter pairs to those with different labels
+    diff_labels = tri_labels(triangle_pairs(:,1)) ~= tri_labels(triangle_pairs(:,2));
+    final_edges = sorted_edges([edge_diffs; false], :);
+    final_edges = final_edges(diff_labels, :);
+    
+    % Draw lines for these boundary edges
+    for i = 1:size(final_edges, 1)
+        vtx_indices = final_edges(i, :);
+        line(surf.vtx(1, vtx_indices), surf.vtx(2, vtx_indices), surf.vtx(3, vtx_indices), 'Color', 'k', 'LineWidth', 3);
+    end
+end
